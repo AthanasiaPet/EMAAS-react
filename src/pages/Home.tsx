c77336e5429cff4e980209984474484be6cc76d6
AthanasiaPet <athanasia.petsa@yahoo.com>
2026-05-19 12:59:25 +0300

    refactor: remove CTA.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Hero from '../components/Hero'
 import MissionStrip from '../components/MissionStrip'
 import About from '../components/About'
 import Goals from '../components/Goals'
-import CTA from '../components/CTA'
+// import CTA from '../components/CTA'
 
 function Home() {
     return (
@@ -11,7 +11,7 @@ function Home() {
             <MissionStrip />
             <About />
             <Goals />
-            <CTA />
+            {/*<CTA />*/}
         </div>
     )
 }
